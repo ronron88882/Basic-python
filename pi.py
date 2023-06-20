403cc69a14167b281a364669710ed26bbc63caa5
ronron88882 <longshishankou9@gmail.com>
2023-06-20 11:51:25 +0900

    Revert "課題終了"

--- pi.py
+++ pi.py
@@ -1,9 +1,7 @@
-text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
-words = text.split()
-word_length = [len(word) for word in words]
+text = """
+    How I want a drink, alcoholic of course, after the heavy chapters involving
+    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
+"""
 
-print(word_length)
+# TODO
 
-answer = ''.join(map(str, word_length))
-
-answer
--- quadratic_formula.py
+++ quadratic_formula.py
@@ -1,38 +1,9 @@
-import math
-import cmath
+a = 1
+b = -6
+c = 9
 
-def coefficient(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    
-    if discriminant < 0:
-     x1 = (-b + cmath.sqrt(discriminant)) / (2 * a)
-     x2 = (-b - cmath.sqrt(discriminant)) / (2 * a)
-    else:
-     x1 = (-b + math.sqrt(discriminant)) / (2 * a)
-     x2 = (-b - math.sqrt(discriminant)) / (2 * a)
-    
-    return x1, x2
+# TODO
+x1 = ...
+x2 = ...
 
-#(1)
-a =1
-b =-6
-c =9
-print(coefficient(a,b,c))
-
-#(2)
-a =1
-b =2
-c =-8
-print(coefficient(a,b,c))
-
-#(3)
-a =8
-b =-6
-c =-35
-print(coefficient(a,b,c))
-
-#(4)
-a =1
-b =0
-c =1
-print(coefficient(a,b,c))
+print(x1, x2)
