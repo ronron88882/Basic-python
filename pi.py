0f0b9aaa1ad0b06b5885dbee0ed1652cb2666076
ronron88882 <longshishankou9@gmail.com>
2023-06-26 14:16:20 +0900

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,7 +1,7 @@
-text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
-"""
+text = "How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."
+words = text.split()
+word_length = [len(word) for word in words]
 
-# TODO
+answer = ''.join(map(str, word_length))
 
+print(answer)
