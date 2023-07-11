092430af76d8f2be6741301f4de01ef7ebdea85a
ronron88882 <longshishankou9@gmail.com>
2023-07-11 16:57:31 +0900

    Update euclid.py

--- euclid.py
+++ euclid.py
@@ -2,4 +2,21 @@ def yc(a, b):
     while b != 0:
         a, b = b, a % b
     return a
+def sosuu(a, b):
+    so = yc(a, b)
+    if so == 1:
+        return True
+    else:
+        return False
 
+#1
+sosuu(10,20)
+yc(10,20)
+
+#2
+sosuu(14,91)
+yc(14,91)
+
+#3
+sosuu(91,14)
+yc(91,14)
